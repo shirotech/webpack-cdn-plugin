d612058c822211c609cc5c3e14108717029ffdfb
Van Nguyen <van@shirotech.com>
2017-06-03 11:55:23 +1000

    feat: remove HtmlWebpackIncludeAssetsPlugin dependency

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 const fs = require('fs');
 const path = require('path');
-const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
 
 const node_modules = module.paths.find(p => fs.existsSync(p));
 const packageJson = 'package.json';
 const paramsRegex = /:([a-z]+)/gi;
 
-class WebpackCdnPlugin extends HtmlWebpackIncludeAssetsPlugin {
+class WebpackCdnPlugin {
 
   constructor({
                 modules, prod = true,
                 prodUrl = '//unpkg.com/:name@:version/:path',
-                devUrl = '/:name/:path', append = false, publicPath
+                devUrl = '/:name/:path', publicPath
   }) {
-    const url = prod ? prodUrl : devUrl;
-
-    super({
-      assets: WebpackCdnPlugin.getAssets(modules, url), append,
-      publicPath: prod ? false : publicPath
-    });
-
     this.modules = modules;
-    this.url = url;
+    this.prefix = prod ? '' : publicPath || '';
+    this.url = prod ? prodUrl : devUrl;
   }
 
   apply(compiler) {
-    super.apply(compiler);
+    this.prefix = compiler.options.publicPath || this.prefix;
+    const getArgs = [this.modules, this.url, this.prefix];
+
+    compiler.plugin('compilation', (compilation) => {
+      compilation.plugin('html-webpack-plugin-before-html-generation', (data, callback) => {
+        data.assets.css = WebpackCdnPlugin._getCss(...getArgs).concat(data.assets.css);
+        data.assets.js = WebpackCdnPlugin._getJs(...getArgs).concat(data.assets.js);
+        callback(null, data);
+      });
+    });
 
     const externals = compiler.options.externals || {};
     this.modules.forEach((p) => {
@@ -35,15 +37,20 @@ class WebpackCdnPlugin extends HtmlWebpackIncludeAssetsPlugin {
     compiler.options.externals = externals;
   }
 
-  static getAssets(modules, url) {
-    return modules.map((p) => {
-      p.version = require(path.join(node_modules, p.name, packageJson)).version;
-      p.path = p.path || require.resolve(p.name).split(`/node_modules/${p.name}/`).pop();
-      return url.replace(paramsRegex, (m, p1) => p[p1]);
-    }).concat(modules.filter((p) => p.style).map((p) => {
+  static _getCss(modules, url, prefix = '') {
+    return modules.filter((p) => p.style).map((p) => {
+      p.version = p.version || require(path.join(node_modules, p.name, packageJson)).version;
       p.path = p.style;
-      return url.replace(paramsRegex, (m, p1) => p[p1]);
-    }));
+      return prefix + url.replace(paramsRegex, (m, p1) => p[p1]);
+    });
+  }
+
+  static _getJs(modules, url, prefix = '') {
+    return modules.map((p) => {
+      p.version = p.version || require(path.join(node_modules, p.name, packageJson)).version;
+      p.path = require.resolve(p.name).split(`/node_modules/${p.name}/`).pop();
+      return prefix + url.replace(paramsRegex, (m, p1) => p[p1]);
+    });
   }
 
 }
