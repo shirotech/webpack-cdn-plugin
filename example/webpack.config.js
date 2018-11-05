867894ce8cdd86a8d6b6bb81dd39d298175c7257
Van Nguyen <van@shirotech.com>
2018-11-06 10:14:38 +1100

    chore: update example

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -2,9 +2,11 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackCdnPlugin = require('../');
+const mode = process.env.NODE_ENV || 'development';
 
 module.exports = {
   entry: path.join(__dirname, 'app.js'),
+  mode,
   output: {
     path: path.join(__dirname, 'dist/assets'),
     publicPath: '/assets',
@@ -17,7 +19,7 @@ module.exports = {
         { name: 'archy' },
         { name: 'jasmine' },
       ],
-      prod: process.env.NODE_ENV === 'production',
+      prod: mode === 'production',
       publicPath: '/node_modules', // override when prod is false
     }),
   ],
